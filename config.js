8859bd7f96bdf51bb8d09ddd31a72f4b0ceb240b
mohdfardeen7007 <162273323+mohdfardeen7007@users.noreply.github.com>
2025-02-06 14:33:09 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,31 +2,50 @@ import { watchFile, unwatchFile } from 'fs'
 import chalk from 'chalk'
 import { fileURLToPath } from 'url'
 import fs from 'fs'
-import fetch from 'node-fetch'
-import axios from 'axios'
 
-// Global variables
-global.owner = [
-  ['918360234087', 'Ｗ Ｈ Ｉ Ｔ Ｅ ４ ４ ４  ? ?', true], 
-  ['923184070915', '𝙎𝘼𝙇𝙈𝘼𝙉 𝘼𝙃𝙈𝘼𝘿 -(𝘼𝙝𝙢𝙢𝙞)', true], 
-  ['233533763772', 'Abraham Dwamena2.0', true],
-  ['233268374753', 'Abraham Dwamena3.0', true]
-] 
+import dotenv from 'dotenv'
+dotenv.config()
 
-global.mods = ['917888755628','923184070915', '233268374753', '923184474176', '233268374753'] 
-global.prems = ['923184070915', '94789481495', '918360234087', '233268374753']
-global.allowed = ['923184070915', '233268374753', '918360234087', '233268374753']
+const ownervb = process.env.OWNERS || "918127875972;Fardeen"
+
+
+const ownerlist = ownervb.split(';');
+
+global.owner = [];
+for (let i = 0; i < ownerlist.length; i += 2) {
+    const owner = [
+        ownerlist[i],            
+        ownerlist[i + 1],         
+        true                        
+    ];
+    global.owner.push(owner);
+}
+
+//global.pairingNumber = "" //put your bot number here
+
+global.mods = ['918127875972', '32460248586']
+global.prems = ['917605902011', '32460248586', '919398758484']
+global.allowed = ['917605902011', '32460248586', '19152999993']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
-global.keysxxx = global.keysZens[Math.floor(global.keysZens.length * Math.random())]
-global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
-global.keysxteam = global.keysxteammm[Math.floor(global.keysxteammm.length * Math.random())]
+global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
+global.keysxteammm = [
+  '29d4b59a4aa687ca',
+  '5LTV57azwaid7dXfz5fzJu',
+  'cb15ed422c71a2fb',
+  '5bd33b276d41d6b4',
+  'HIRO',
+  'kurrxd09',
+  'ebb6251cc00f9c63',
+]
+global.keysxteam = keysxteammm[Math.floor(keysxteammm.length * Math.random())]
 global.keysneoxrrr = ['5VC9rvNx', 'cfALv5']
-global.keysneoxr = global.keysneoxrrr[Math.floor(global.keysneoxrrr.length * Math.random())]
+global.keysneoxr = keysneoxrrr[Math.floor(keysneoxrrr.length * Math.random())]
 global.lolkeysapi = ['GataDios']
-global.beta = 'mLxstUwm'
 
 global.APIs = {
-  xteam: 'https://api.xteam.xyz', 
+  // API Prefix
+  // name: 'https://website'
+  xteam: 'https://api.xteam.xyz',
   dzx: 'https://api.dhamzxploit.my.id',
   lol: 'https://api.lolhuman.xyz',
   violetics: 'https://violetics.pw',
@@ -36,40 +55,41 @@ global.APIs = {
   akuari2: 'https://apimu.my.id',
   nrtm: 'https://fg-nrtm.ddns.net',
   bg: 'http://bochil.ddns.net',
-  fgmods: 'https://api-fgmods.ddns.net'
+  fgmods: 'https://api-fgmods.ddns.net',
 }
-
-global.APIKeys = { 
+global.APIKeys = {
+  // APIKey Here
+  // 'https://website': 'apikey'
   'https://api.xteam.xyz': 'd90a9e986e18778b',
   'https://api.lolhuman.xyz': '85faf717d0545d14074659ad',
-  'https://api.neoxr.my.id': global.keysneoxr,
+  'https://api.neoxr.my.id': `${keysneoxr}`,
   'https://violetics.pw': 'beta',
-  'https://zenzapis.xyz': global.keysxxx, 
-  'https://api-fgmods.ddns.net': 'fg-dylux'
+  'https://zenzapis.xyz': `${keysxxx}`,
+  'https://api-fgmods.ddns.net': 'fg-dylux',
 }
 
 // Sticker WM
-global.botname = '𝘟𝘭𝘪𝘤𝘰𝘯 𝘝𝘰𝘭 2'
+global.botname = process.env.BOTNAME
 global.premium = 'true'
-global.packname = '𝙏𝙚𝙖𝙢 𝙓𝙇𝙄𝘾𝙊𝙉' 
-global.author = '@𝘮𝘥𝘹𝘭𝘪𝘤𝘰𝘯𝘷2' 
-global.menuvid = 'https://files.catbox.moe/yu3j54.mp4'
-global.igfg = '▢✓ Follow Our channel\nhttps://whatsapp.com/channel/0029VaMGgVL3WHTNkhzHik3c\n' 
-global.dygp = 'https://chat.whatsapp.com/BfH0KLkICn2BjmGFMRcGMW'
-global.fgsc = 'https://github.com/salmanytofficial/XLICON-V2-MD' 
-global.fgyt = 'https://youtube.com/@s4salmanyt'
-global.fgpyp = 'https://youtube.com/@s4salmanyt'
-global.fglog = 'XLICON.jpg' 
-global.thumb = fs.readFileSync('./XLICON.jpg')
+global.packname = '▒ ̶▍ 𝝙︩𝗠̸̶ׅ𝕭𝕹𝐎̸𝗦̸̷̶𝚺̲  ྀི       𝆭 '
+global.author = '▒ ̶▍ 𝝙︩𝗠̸̶ׅ𝕭𝕹𝐎̸𝗦̸̷̶𝚺̲  ྀི       𝆭 '
+global.menuvid = 'https://i.imgur.com/AjDzGgG.mp4'
+global.igfg = '▢ Follow on Instagram\nhttps\n'
+global.dygp = 'https://chat.whatsapp.com'
+global.fgsc = 'https://github.com/Guru322/GURU-BOT'
+global.fgyt = 'https://youtube.com/@Asliguru'
+global.fgpyp = 'https://youtube.com/@Asliguru'
+global.fglog = 'https://raw.githubusercontent.com/Guru322/api/Guru/guru.jpg'
+global.thumb = fs.readFileSync('./Assets/Gurulogo.jpg')
 
-global.wait = '*🕣 _XLICON IS LOADING..._*\n*▰▰▰▱▱▱▱▱⭐*'
+global.wait = '*⌛ _Wait Kr..._*\n*▰▰▰▱▱▱▱▱*'
 global.rwait = '⌛'
 global.dmoji = '🤭'
-global.done = '✔️'
-global.error = '❌' 
-global.xmoji = '🔥' 
+global.done = '✅'
+global.error = '❌'
+global.xmoji = '🔥'
 
-global.multiplier = 69 
+global.multiplier = 69
 global.maxwarn = '3'
 
 let file = fileURLToPath(import.meta.url)
