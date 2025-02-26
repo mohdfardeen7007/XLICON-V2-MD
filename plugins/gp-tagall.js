9616e000cb362e21f7c70e12d7d12b697c3caa72
mohdfardeen7007 <162273323+mohdfardeen7007@users.noreply.github.com>
2025-02-26 12:00:34 +0530

    Update gp-tagall.js

--- plugins/gp-tagall.js
+++ plugins/gp-tagall.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, text, participants, isAdmin, isOwner, groupMetadata }) => {
     let users = participants.map(u => u.id).filter(v => v !== conn.user.jid)
-    m.reply(`͏📜︩︪ 𝐆𝗋ⱺυρ : *${groupMetadata.subject}*\n\n ͏🌖︩︪𝐌𝖾ꭑᑲ𝖾𝗋𝗌 : *${participants.length}*${text ? `\n 🌟𝂄𝐌𝖾𝗌𝗌α𝗀𝖾 : ${text}\n` : ''}\n\n͏██  ͏██ ͏ ͏. 𝐓ꭤ̱ꬶꤦᥣі𝗌̱𝗍 .  ͏██ ͏ ͏██ ͏\n` + users.map(v => '🥮ᩧᩙᩪᩩ̶̷ ㅤ ͟ ͟ ͟ ͟  @' + v.replace(/@.+/, '')).join`\n` + '\n *ׅ ׄ𖫑ᩞ ፝֟᷼͡ 𖫑ᩞ ׄ ׅ- Ɗყi̫ɳ𝐠 i̫ɳຮi̫d͟͟͠ᥱ , Տ𑜀i̫ᥣi̫ɳ𝐠 𑄝ׁ𝐮t͟ຮi̫d͟͟͠ᥱ. 𝗹̶͟🌙᪶ 𝆬 ׅ ꒱* ', null, {
+    m.reply(`͏📜︩︪ 𝐆𝗋ⱺυρ : *${groupMetadata.subject}*\n\n🌖︩︪𝐌𝖾ꭑᑲ𝖾𝗋𝗌 : *${participants.length}*${text ? `\n🌟𝂄𝐌𝖾𝗌𝗌α𝗀𝖾 : ${text}\n` : ''}\n\n͏██  ͏██ ͏ ͏. 𝐓ꭤ̱ꬶꤦᥣі𝗌̱𝗍 .  ͏██ ͏ ͏██ ͏\n` + users.map(v => '🥮ᩧᩙᩪᩩ̶̷ ㅤ ͟ ͟ ͟ ͟  @' + v.replace(/@.+/, '')).join`\n` + '\n *ׅ ׄ𖫑ᩞ ፝֟᷼͡ 𖫑ᩞ ׄ ׅ- Ɗყi̫ɳ𝐠 i̫ɳຮi̫d͟͟͠ᥱ , Տ𑜀i̫ᥣi̫ɳ𝐠 𑄝ׁ𝐮t͟ຮi̫d͟͟͠ᥱ. 𝗹̶͟🌙᪶ 𝆬 ׅ ꒱* ', null, {
         mentions: users
     })
 }
