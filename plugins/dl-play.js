250a21d7da3f3437ecd04a1799bc2fdf6174458e
mohdfardeen7007 <162273323+mohdfardeen7007@users.noreply.github.com>
2025-03-19 17:00:11 +0530

    Update dl-play.js

--- plugins/dl-play.js
+++ plugins/dl-play.js
@@ -19,7 +19,7 @@ const handler = async (m, {
     conn.GURUPLAY = conn.GURUPLAY ? conn.GURUPLAY : {};
     await conn.reply(m.chat, wait, m);
     const result = await searchAndDownloadMusic(text);
-    const infoText = `🔰──『 *XLICON-V2 PLAYER* 』── 🔰`;
+    const infoText = `𝐅𝖾ᥣ𝗂𝗑 𝐏ᥣα𝗒𝖾𝗋`;
 
 const orderedLinks = result.allLinks.map((link, index) => {
     const sectionNumber = index + 1;
@@ -100,9 +100,9 @@ handler.before = async (m, {
     }
 };
 
-handler.help = ["play2"];
+handler.help = ["play"];
 handler.tags = ["downloader"];
-handler.command = /^(play2)$/i;
+handler.command = ["play"];
 handler.limit = true;
 export default handler;
 
