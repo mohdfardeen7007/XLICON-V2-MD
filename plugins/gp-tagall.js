17d3e6ba0aa15a93542b9372602fe004c8330ba0
mohdfardeen7007 <162273323+mohdfardeen7007@users.noreply.github.com>
2025-03-19 16:56:23 +0530

    Update gp-tagall.js

--- plugins/gp-tagall.js
+++ plugins/gp-tagall.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, text, participants, isAdmin, isOwner, groupMetadata }) => {
     let users = participants.map(u => u.id).filter(v => v !== conn.user.jid)
-    m.reply(`▢ Group : *${groupMetadata.subject}*\n▢ Members : *${participants.length}*${text ? `\n▢ Message : ${text}\n` : ''}\n┌───⊷ *MENTIONS*\n` + users.map(v => '▢ @' + v.replace(/@.+/, '')).join`\n` + '\n└──✪ XLICON┃ ᴮᴼᵀ ✪──', null, {
+    m.reply(`͏📜︩︪ 𝐆𝗋ⱺυρ : *${groupMetadata.subject}*\n\n🌖︩︪𝐌𝖾ꭑᑲ𝖾𝗋𝗌 : *${participants.length}*${text ? `\n🌟𝂄𝐌𝖾𝗌𝗌α𝗀𝖾 : ${text}\n` : ''}\n\n͏██  ͏██ ͏ ͏. 𝐓ꭤ̱ꬶꤦᥣі𝗌̱𝗍 .  ͏██ ͏ ͏██ ͏\n` + users.map(v => '🥮ᩧᩙᩪᩩ̶̷ ㅤ ͟ ͟ ͟ ͟  @' + v.replace(/@.+/, '')).join`\n` + '\n *ׅ ׄ𖫑ᩞ ፝֟᷼͡ 𖫑ᩞ ׄ ׅ- Ɗყi̫ɳ𝐠 i̫ɳຮi̫d͟͟͠ᥱ , Տ𑜀i̫ᥣi̫ɳ𝐠 𑄝ׁ𝐮t͟ຮi̫d͟͟͠ᥱ. 𝗹̶͟🌙᪶ 𝆬 ׅ ꒱* ', null, {
         mentions: users
     })
 }
@@ -8,7 +8,7 @@ let handler = async (m, { conn, text, participants, isAdmin, isOwner, groupMetad
 handler.help = ['tagall']
 handler.tags = ['group']
 handler.command = ['tagall']
-handler.admin = true
+handler.admin = false
 handler.group = true
 
 export default handler
